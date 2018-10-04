2241e0c87095f5ae3944bdedd4d7f93dc3f0a04e
kidus11 <kiduel@gmail.com>
2018-10-03 20:32:22 -0700

    - ready for submission

--- jokeDisplayer/src/main/java/com/example/android/jokedisplayer/JokeDisplayingActivity.java
+++ jokeDisplayer/src/main/java/com/example/android/jokedisplayer/JokeDisplayingActivity.java
@@ -2,17 +2,24 @@ package com.example.android.jokedisplayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class JokeDisplayingActivity extends AppCompatActivity {
 
+    TextView textView;
+    public String JOKE_KEY = "joke_key";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_displaying);
+        textView = (TextView) findViewById(R.id.joke_text_view);
 
         Bundle joke = getIntent().getExtras();
         String joke_string = joke.getString("joke_key");
-        Toast.makeText(this, joke_string, Toast.LENGTH_SHORT).show();
+        textView.setText(joke_string);
+
     }
 }
--- tellJoke/src/main/java/com/example/android/telljoke/Joke.java
+++ tellJoke/src/main/java/com/example/android/telljoke/Joke.java
@@ -1,8 +1,7 @@
 package com.example.android.telljoke;
 
 public class Joke {
-    String joke = "The cat qeb the rat, because silerabat ";
-
+    String joke = "An Android app walks into a bar. Bartender asks, “Can I get you a drink?” The app looks disappointed and says, “That wasn’t my intent.";
     public String getJoke() {
         return joke;
     }
